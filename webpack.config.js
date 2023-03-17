b2c6ff4914b1f2e0d8a449813a325fc698b8b504
posternaka <nik.petrenko97@gmail.com>
2023-03-18 00:48:11 +0300

    setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 3000,
+    }
+};
+
+module.exports = ({develop}) => ({
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].bundle.js",
+        clean: true,
+    },
+    mode: develop ? 'development' : 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "payment",
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(|png|svg)$/i,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+        ]
+    },
+    ...devServer(develop),
+});
